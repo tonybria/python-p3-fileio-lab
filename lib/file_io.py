fe4e25371450ac23b202560d027d5e6d596cf861
tony-brian <btoni3823@gmail.com>
2023-09-02 16:03:53 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
-def write_file(file_name, file_content):
-    pass
+def test_write_file(tmp_path):
+    """Test write_file()"""
+    file_name = tmp_path / "test_file"
+    file_content = "This is a test content."
 
-def append_file(file_name, append_content):
-    pass
+def test_append_file(tmp_path):
+    """Test append_file()"""
 
-def read_file(file_name):
-    pass
+    file_name = tmp_path / "test_file"
+    file_content = "This is a test content."
+    append_content = "\nAppended content."
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            content = f.read()
+        return content
+    except FileNotFoundError:
+        return None  # Return None to indicate that the file was not found
